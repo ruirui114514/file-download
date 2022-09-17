aed5ef5ecaea421157df5a0ad182f505e357c174
ruirui114514 <113703848+ruirui114514@users.noreply.github.com>
2022-09-17 20:07:17 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,10 +8,35 @@ int main()
 	getline(cin,filename);
 	cout<<"url:";
 	getline(cin,url);
-	fout<<"cd /d %~dp0\npowershell curl -o \""<<filename<<"\" \""<<url<<"\"";
+	fout<<"@echo off\ncd /d %~dp0\npowershell curl -o \"";
+	for(int i=0;i<filename.size();i++)
+	{
+		if(filename[i]=='&')
+		{
+			fout<<"\"&\"";
+		}
+		else
+		{
+			fout<<filename[i];
+		}
+	}
+	fout<<"\" \"";
+	for(int i=0;i<url.size();i++)
+	{
+		if(url[i]=='&')
+		{
+			fout<<"\"&\"";
+		}
+		else
+		{
+			fout<<url[i];
+		}
+	}
+	fout<<"\"";
 	fout.close();
 	system("test.bat");
 	system("del test.bat");
-	cout<<filename<<"ok!";
+	cout<<filename<<",ok!"<<endl;
+	system("pause") ;
 	return 0;
 }
